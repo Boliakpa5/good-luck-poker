8a5c2686cc0a049c5034c473f8edede5d62cf49d
Francois Paillon <francoispaillon@hotmail.fr>
2022-11-29 12:46:18 +0100

    current version

--- app/controllers/table_hands_controller.rb
+++ app/controllers/table_hands_controller.rb
@@ -1,12 +1,15 @@
 class TableHandsController < ApplicationController
   # STATUSES = %w[bet flop turn river end]
-  before_action :set_poker_table, only: [:start]
-  def start
+  before_action :set_poker_table, only: [:create]
+
+  def create
     @cards = TableHand::CARDS
     @tablehand = TableHand.new
     @tablehand.poker_table = @poker_table
     @tablehand.current_call_amount = 2 * @tablehand.poker_table.small_blind
-    @tablehand.first_player_position = 1
+    thislasttablehand = TableHand.where(poker_table: @poker_table).last
+    @tablehand.first_player_position = thislasttablehand.first_player_position.empty? ? 1 : thislasttablehand.first_player_position + 1
+    @tablehand.current_player_position = (@tablehand.first_player_position + 2) % @poker_table.max_players
     @tablehand.status = TableHand::STATUSES[0]
     @poker_table.players.where(active: true).each do |player|
       player_hand = PlayerHand.new
@@ -16,13 +19,14 @@ class TableHandsController < ApplicationController
       player_hand.table_hand = @tablehand
       player_hand.save
     end
+    @tablehand.save
   end
 
 
   private
 
   def set_poker_table
-    @poker_table = params[:poker_table_id]
+    @poker_table = PokerTable.find(params[:poker_table_id])
   end
 
   def pick_a_card
