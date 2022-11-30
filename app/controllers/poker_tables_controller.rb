f4b8441a085cf9c57daf87e62b3112600448f4d1
Francois Paillon <francoispaillon@hotmail.fr>
2022-11-30 17:04:11 +0100

    ok

--- app/controllers/poker_tables_controller.rb
+++ app/controllers/poker_tables_controller.rb
@@ -1,5 +1,5 @@
 class PokerTablesController < ApplicationController
-  skip_before_action :authenticate_user!
+  skip_before_action :authenticate_user!, only: [:index]
 
   def index
     @poker_tables = PokerTable.all
