87f276aee8b83cce7a0e9ee8b19343929fe8f199
Francois Paillon <francoispaillon@hotmail.fr>
2022-12-01 12:42:06 +0100

    game functionnal (without winner tho)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_30_143121) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_01_112552) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -51,6 +51,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_30_143121) do
     t.boolean "folded", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "winner", default: false
     t.index ["player_id"], name: "index_player_hands_on_player_id"
     t.index ["table_hand_id"], name: "index_player_hands_on_table_hand_id"
   end
