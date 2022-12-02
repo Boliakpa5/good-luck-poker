3b76eb1a5287e71930aaaec159f177b04e925ea4
Etienne Klepal <etienne.klepal@gmail.com>
2022-12-02 11:24:28 +0100

    final front progression

--- app/models/player.rb
+++ app/models/player.rb
@@ -4,7 +4,7 @@ class Player < ApplicationRecord
   has_many :player_hands
   has_many :table_hands, through: :player_hands
   validates :stack, :position, presence: true
-  validates :position, numericality: { in: 1..9 }
+  validates :position, numericality: { in: 1..9 } #, uniqueness: { scope: [:poker_table, where(active: true)] }
   # Pour utiliser un .active partout
   scope :active, -> { where(active: true) }
 end
