136746d19ae3345c1bd5e922d1789bece6a6f9f5
Francois Paillon <francoispaillon@hotmail.fr>
2022-12-05 18:18:59 +0100

    ratio

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -35,7 +35,7 @@ class ApplicationController < ActionController::Base
     players = Player.where(user: current_user)
     # All time counter
     players.each do |player|
-      player.player_hands.where(folded: false).each do |player_hand|
+      player.player_hands.where(folded: false).where.not(combination: nil).each do |player_hand|
         hand_counter += 1
         pair_counter += 1 if player_hand.combination.first == 1
         double_counter += 1 if player_hand.combination.first == 2
@@ -51,29 +51,32 @@ class ApplicationController < ActionController::Base
     # anti first time crash
     hand_counter = 1 if hand_counter.zero?
     # means
-    pair_mean = pair_counter / hand_counter
-    double_mean = double_counter / hand_counter
-    three_mean = three_counter / hand_counter
-    straight_mean = straight_counter / hand_counter
-    flush_mean = flush_counter / hand_counter
-    full_mean = full_counter / hand_counter
-    four_mean = four_counter / hand_counter
-    straight_flush_mean = straight_flush_counter / hand_counter
-    royal_mean = royal_counter / hand_counter
+    pair_mean = pair_counter / hand_counter.to_f
+    double_mean = double_counter / hand_counter.to_f
+    three_mean = three_counter / hand_counter.to_f
+    straight_mean = straight_counter / hand_counter.to_f
+    flush_mean = flush_counter / hand_counter.to_f
+    full_mean = full_counter / hand_counter.to_f
+    four_mean = four_counter / hand_counter.to_f
+    straight_flush_mean = straight_flush_counter / hand_counter.to_f
+    royal_mean = royal_counter / hand_counter.to_f
 
     # ratios
-    pair_ratio = pair_mean / 42.2569027611
-    double_ratio = double_mean / 4.7539015606
-    three_ratio = three_mean / 2.1128451381
-    straight_ratio = straight_mean / 0.3924646782
-    flush_ratio = flush_mean / 0.1965401545
-    full_ratio = full_mean / 0.144057623
-    four_ratio = four_mean / 0.0240096038
-    straight_flush_ratio = straight_flush_mean / 0.0013851695
-    royal_ratio = royal_mean / 0.0001539077
+    pair_moy = 42.2569027611
+    double_moy = 4.7539015606
+    three_moy = 2.1128451381
+    straight_moy = 0.3924646782
+    flush_moy = 0.1965401545
+    full_moy = 0.144057623
+    four_moy = 0.0240096038
+    straight_flush_moy = 0.0013851695
+    royal_moy = 0.0001539077
+
+    # ecarts a la moyenne au carré
+    pair_sigma = ((pair_mean - pair_moy).abs)**2
 
     # main ratio
-    main_ratio = (pair_ratio + double_ratio + three_ratio + straight_ratio + flush_ratio + full_ratio + four_ratio + straight_flush_ratio + royal_ratio) / 9
+    main_ratio = (pair_ratio + double_ratio + three_ratio + straight_ratio + flush_ratio + full_ratio + four_ratio + straight_flush_ratio + royal_ratio) / 9.to_f
 
     # saving
     current_user.update(luck_ratio: main_ratio)
