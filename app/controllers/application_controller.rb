e25be700f4377e63afa443e9226e6c5b325d1ab7
Francois Paillon <francoispaillon@hotmail.fr>
2022-12-08 11:02:33 +0100

    trends and daily ratio

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -52,6 +52,48 @@ class ApplicationController < ActionController::Base
     # anti first time crash
     return current_user.update(luck_ratio: 1) if hand_counter.zero?
 
+    # main ratio
+    main_ratio = calculateratios(hand_counter, pair_counter, double_counter, three_counter, straight_counter, flush_counter, full_counter, four_counter, straight_flush_counter, royal_counter)
+    # saving
+    current_user.update(luck_ratio: main_ratio)
+
+    hand_counter = 0
+    pair_counter = 0
+    double_counter = 0
+    three_counter = 0
+    straight_counter = 0
+    flush_counter = 0
+    full_counter = 0
+    four_counter = 0
+    straight_flush_counter = 0
+    royal_counter = 0
+
+    # Daily counter
+    players.select { |player| player.created_at >= Date.today }.each do |player|
+      player.player_hands.where.not(combination: nil).each do |player_hand|
+        hand_counter += 1
+        # high_counter += 1 if player_hand.combination.first == 0
+        pair_counter += 1 if player_hand.combination.first == 1
+        double_counter += 1 if player_hand.combination.first == 2
+        three_counter += 1 if player_hand.combination.first == 3
+        straight_counter += 1 if player_hand.combination.first == 4
+        flush_counter += 1 if player_hand.combination.first == 5
+        full_counter += 1 if player_hand.combination.first == 6
+        four_counter += 1 if player_hand.combination.first == 7
+        straight_flush_counter += 1 if player_hand.combination.first == 8
+        royal_counter += 1 if player_hand.combination.first == 9
+      end
+    end
+
+    # daily ratio
+    daily_ratio = calculateratios(hand_counter, pair_counter, double_counter, three_counter, straight_counter, flush_counter, full_counter, four_counter, straight_flush_counter, royal_counter)
+    # saving
+    (daily_ratio >= current_user.daily_ratio ? current_user.update(increasing_luck: true) : current_user.update(increasing_luck: false)) unless current_user.daily_ratio.nil?
+    current_user.update(daily_ratio: daily_ratio)
+  end
+
+  def calculateratios(hand_counter, pair_counter, double_counter, three_counter, straight_counter, flush_counter, full_counter, four_counter, straight_flush_counter, royal_counter)
+
     # means
     pair_mean = pair_counter / hand_counter.to_f
     double_mean = double_counter / hand_counter.to_f
@@ -75,9 +117,6 @@ class ApplicationController < ActionController::Base
     royal_ratio = (royal_mean - 0.000001539077) + 1
 
     # main ratio
-    main_ratio = ((pair_ratio * 10) + (double_ratio *5) + three_ratio + straight_ratio + flush_ratio + full_ratio + four_ratio + straight_flush_ratio + royal_ratio) / 24.to_f
-
-    # saving
-    current_user.update(luck_ratio: main_ratio)
+    ((pair_ratio * 10) + (double_ratio * 5) + three_ratio + straight_ratio + flush_ratio + full_ratio + four_ratio + straight_flush_ratio + royal_ratio) / 24.to_f
   end
 end
